0c13db62bace776c10d180cc2f3c890a492bd309
nishantSanandiyaAgile <nishant.sanandiya@agileinfoways.com>
2024-02-14 12:39:18 +0530

    implement location update in background for android and ios

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,52 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: '@react-native-community',
+  plugins: ['react', 'unused-imports', 'simple-import-sort'],
+  rules: {
+    // 'react/no-array-index-key': 'error',
+    'react/no-children-prop': 'error',
+    'no-unused-expressions': 'error',
+    'react-native/no-unused-styles': 'error',
+    'react-native/no-inline-styles': 'error',
+    // 'react-native/no-raw-text': ['error', {skip: ['CustomText']}],
+    'react-native/no-single-element-style-arrays': 'warn',
+    'object-curly-spacing': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'no-multiple-empty-lines': 'error',
+    'react-hooks/exhaustive-deps': 0,
+    'unused-imports/no-unused-imports': 'error',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
+  overrides: [
+    // override "simple-import-sort" config
+    {
+      files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+      rules: {
+        'simple-import-sort/imports': [
+          'error',
+          {
+            groups: [
+              // Packages `react` related packages come first.
+              ['^react', '^@?\\w'],
+              // Internal packages.
+              ['^(@|components)(/.*|$)'],
+              ['^~'],
+              // Side effect imports.
+              ['^\\u0000'],
+              // Parent imports. Put `..` last.
+              ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+              // Other relative imports. Put same-folder imports and `.` last.
+              ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+              // Style imports.
+              ['^.+\\.?(css)$'],
+            ],
+          },
+        ],
+      },
+    },
+  ],
 };
--- App.tsx
+++ App.tsx
@@ -1,117 +1,255 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {Button, StatusBar, StyleSheet, View} from 'react-native';
+import {PERMISSIONS, request, requestMultiple} from 'react-native-permissions';
+import Geolocation from '@react-native-community/geolocation';
+import {watchPosition} from 'react-native-geolocation-service';
+import ReactNativeForegroundService from '@supersami/rn-foreground-service';
+import RNLocation from 'react-native-location';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const requestPermissionsHandlerWithCommunityGeoLocation = async () => {
+    try {
+      const response2 = await requestMultiple([
+        PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+        PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION,
+      ]);
+      const response = await request(
+        PERMISSIONS.ANDROID.ACCESS_BACKGROUND_LOCATION,
+      );
+      console.log('response for background Location Permission :- ', response);
+      console.log(
+        'response fo ACCESS_FINE_LOCATION ACCESS_COARSE_LOCATION Permission :- ',
+        response2,
+      );
+      Geolocation.setRNConfiguration({
+        authorizationLevel: 'always',
+        enableBackgroundLocationUpdates: true,
+        locationProvider: 'auto',
+        skipPermissionRequests: false,
+      });
+    } catch (err) {
+      console.log('Error in request :- ', err);
+    }
+  };
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const onLocationPressHandlerWithCommunityGeoLocation = async () => {
+    await requestPermissionsHandlerWithCommunityGeoLocation();
+    Geolocation.watchPosition(
+      response => {
+        console.log('Location Updates :- ', response);
+      },
+      error => {
+        console.log('Error in Location Updates :- ', error);
+      },
+      {
+        interval: 10000,
+        distanceFilter: 20,
+        useSignificantChanges: true,
+        enableHighAccuracy: true,
+        maximumAge: 30000,
+      },
+    );
+  };
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+  const requestPermissionHandlerWithGeolocationService = async () => {
+    try {
+      const response2 = await requestMultiple([
+        PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+        PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION,
+      ]);
+      const response = await request(
+        PERMISSIONS.ANDROID.ACCESS_BACKGROUND_LOCATION,
+      );
+      console.log('response for background Location Permission :- ', response);
+      console.log(
+        'response fo ACCESS_FINE_LOCATION ACCESS_COARSE_LOCATION Permission :- ',
+        response2,
+      );
+    } catch (err) {
+      console.log('Error in request :- ', err);
+    }
+  };
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const onLocationPressHandlerWithGeoLocationServices = async () => {
+    await requestPermissionHandlerWithGeolocationService();
+    watchPosition(
+      position => {
+        console.log('Location Updates :- ', position);
+      },
+      error => {
+        console.log('Error in Location Updates :- ', error);
+      },
+      {
+        enableHighAccuracy: true,
+        accuracy: {
+          android: 'high',
+          ios: 'best',
+        },
+        showLocationDialog: true,
+        showsBackgroundLocationIndicator: true,
+        useSignificantChanges: true,
+        interval: 10000,
+        distanceFilter: 20,
+        forceRequestLocation: true,
+      },
+    );
+  };
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const requestPermissionsHandlerWithForegroundService = async () => {
+    try {
+      const response3 = await request(PERMISSIONS.ANDROID.POST_NOTIFICATIONS);
+      const response2 = await requestMultiple([
+        PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+        PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION,
+      ]);
+      const response = await request(
+        PERMISSIONS.ANDROID.ACCESS_BACKGROUND_LOCATION,
+      );
+      console.log('response3 :- ', response3);
+      console.log('response for background Location Permission :- ', response);
+      console.log(
+        'response fo ACCESS_FINE_LOCATION ACCESS_COARSE_LOCATION Permission :- ',
+        response2,
+      );
+    } catch (err) {
+      console.log('Error in request :- ', err);
+    }
+  };
+
+  const foregroundWatchHandler = () => {
+    Geolocation.getCurrentPosition(
+      response => {
+        console.log('Response from foregroundWatchHandler :- ', response);
+      },
+      err => {
+        console.log('err from foregroundWatchHandler :- ', err);
+      },
+      {
+        distanceFilter: 20,
+        useSignificantChanges: true,
+        enableHighAccuracy: true,
+        maximumAge: 30000,
+      },
+    );
+  };
+
+  const startForegroundServiceHandler = async () => {
+    await requestPermissionsHandlerWithForegroundService();
+    ReactNativeForegroundService.add_task(() => foregroundWatchHandler(), {
+      delay: 10000,
+      onSuccess: () => {
+        console.log('Service Successfully Run');
+      },
+      onLoop: true,
+      taskId: 'locationTask',
+      onError: e => console.log(`Error in Service logging:`, e),
+    });
+    await ReactNativeForegroundService.start({
+      id: 1,
+      title: 'Foreground Service',
+      message: 'you are online!',
+      importance: '2',
+      icon: 'ic_launcher_foreground',
+    });
+  };
+
+  const stopForegroundServiceHandler = async () => {
+    await ReactNativeForegroundService.stopAll();
+  };
+
+  const iosWatchPositionPermissionHandler = async () => {
+    const response = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+    console.log('response of Permissions :- ', response);
+    Geolocation.setRNConfiguration({
+      authorizationLevel: 'always',
+      enableBackgroundLocationUpdates: true,
+      locationProvider: 'auto',
+      skipPermissionRequests: false,
+    });
+    await Geolocation.requestAuthorization();
+
+    // RNLocation.configure({
+    //   activityType: 'automotiveNavigation',
+    //   allowsBackgroundLocationUpdates: true,
+    //   desiredAccuracy: {
+    //     ios: 'bestForNavigation',
+    //   },
+    //   pausesLocationUpdatesAutomatically: false,
+    //   showsBackgroundLocationIndicator: true,
+    //   distanceFilter: 10,
+    // });
+    // RNLocation.requestPermission({
+    //   ios: 'always',
+    //   android: {
+    //     detail: 'fine',
+    //   },
+    // });
+  };
+
+  const iosWatchPositionHandler = async () => {
+    Geolocation.watchPosition(
+      response => {
+        console.log('Location Updates in ios :- ', response);
+      },
+      error => {
+        console.log('Error in Location Updates ios :- ', error);
+      },
+      {
+        // interval: 10000,
+        distanceFilter: 20,
+        useSignificantChanges: true,
+        enableHighAccuracy: true,
+      },
+    );
+
+    // RNLocation.subscribeToLocationUpdates(locations => {
+    //   console.log('locations in subscribeToLocationUpdates :- ', locations);
+    // });
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <View style={styles.mainView}>
+      <StatusBar />
+      <Button
+        title="Get Updates With Community GeoLocation"
+        onPress={onLocationPressHandlerWithCommunityGeoLocation}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+      <Button
+        title="Get Updates With GeoLocation Service"
+        onPress={onLocationPressHandlerWithGeoLocationServices}
+      />
+      <View style={styles.breakLine} />
+      <Button
+        title="Start Foreground Service"
+        onPress={startForegroundServiceHandler}
+      />
+      <Button
+        title="Stop Foreground Service"
+        onPress={stopForegroundServiceHandler}
+      />
+      <View style={styles.breakLine} />
+      <Button
+        title="IOS Callback Permission"
+        onPress={iosWatchPositionPermissionHandler}
+      />
+      <Button title="IOS callbacks Service" onPress={iosWatchPositionHandler} />
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  mainView: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    rowGap: 20,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  breakLine: {
+    marginVertical: 10,
+    height: 2,
+    width: '100%',
+    backgroundColor: 'black',
   },
 });
 
